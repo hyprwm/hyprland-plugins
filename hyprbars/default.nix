a7cd7d39aa240f6c9e671655c813e8b20770e248
nehrbash <stephen.nehrbass@infinitetactics.com>
2023-05-01 15:29:29 -0400

    hyprbars: add default.nix

---
+++ hyprbars/default.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  stdenv,
+  hyprland,
+}:
+stdenv.mkDerivation {
+  name = "hyprbars";
+  version = "0.1";
+  src = ./.;
+
+  inherit (hyprland) nativeBuildInputs;
+
+  buildInputs = [hyprland] ++ hyprland.buildInputs;
+
+  meta = with lib; {
+    homepage = "https://github.com/hyprwm/hyprland-plugins";
+    description = "Hyprland window title plugin";
+    license = licenses.bsd3;
+    platforms = platforms.linux;
+  };
+}
