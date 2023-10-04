efd7a19069a4c41bec2efaf74b8e784f47cd5763
Mihai Fufezan <fufexan@protonmail.com>
2023-10-04 12:08:57 +0300

    nix: use gcc13Stdenv for plugins

--- flake.nix
+++ flake.nix
@@ -11,13 +11,22 @@
     withPkgsFor = fn: nixpkgs.lib.genAttrs (builtins.attrNames hyprland.packages) (system: fn system nixpkgs.legacyPackages.${system});
   in {
     packages = withPkgsFor (system: pkgs: {
-      borders-plus-plus = pkgs.callPackage ./borders-plus-plus {inherit (hyprland.packages.${system}) hyprland;};
-      csgo-vulkan-fix = pkgs.callPackage ./csgo-vulkan-fix {inherit (hyprland.packages.${system}) hyprland;};
-      hyprbars = pkgs.callPackage ./hyprbars {inherit (hyprland.packages.${system}) hyprland;};
+      borders-plus-plus = pkgs.callPackage ./borders-plus-plus {
+        inherit (hyprland.packages.${system}) hyprland;
+        stdenv = pkgs.gcc13Stdenv;
+      };
+      csgo-vulkan-fix = pkgs.callPackage ./csgo-vulkan-fix {
+        inherit (hyprland.packages.${system}) hyprland;
+        stdenv = pkgs.gcc13Stdenv;
+      };
+      hyprbars = pkgs.callPackage ./hyprbars {
+        inherit (hyprland.packages.${system}) hyprland;
+        stdenv = pkgs.gcc13Stdenv;
+      };
     });
 
     devShells = withPkgsFor (system: pkgs: {
-      default = pkgs.mkShell {
+      default = pkgs.mkShell.override {stdenv = pkgs.gcc13Stdenv;} {
         name = "hyprland-plugins";
         buildInputs = [hyprland.packages.${system}.hyprland];
         inputsFrom = [hyprland.packages.${system}.hyprland];
