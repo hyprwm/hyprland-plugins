7ff88838f80252b8ca6f3ea6aadd5efc51bc3930
Winter H. <a2405392127a@gmail.com>
2023-10-10 14:17:28 -0700

    added border ratio condig option

--- borders-plus-plus/borderDeco.cpp
+++ borders-plus-plus/borderDeco.cpp
@@ -1,12 +1,11 @@
 #include "borderDeco.hpp"
-
 #include <hyprland/src/Compositor.hpp>
 #include <hyprland/src/Window.hpp>
-
 #include "globals.hpp"
 
-CBordersPlusPlus::CBordersPlusPlus(CWindow* pWindow) : IHyprWindowDecoration(pWindow), m_pWindow(pWindow) {
-    m_vLastWindowPos  = pWindow->m_vRealPosition.vec();
+CBordersPlusPlus::CBordersPlusPlus(CWindow *pWindow)
+    : IHyprWindowDecoration(pWindow), m_pWindow(pWindow) {
+    m_vLastWindowPos = pWindow->m_vRealPosition.vec();
     m_vLastWindowSize = pWindow->m_vRealSize.vec();
 }
 
@@ -18,92 +17,117 @@ SWindowDecorationExtents CBordersPlusPlus::getWindowDecorationExtents() {
     return m_seExtents;
 }
 
-void CBordersPlusPlus::draw(CMonitor* pMonitor, float a, const Vector2D& offset) {
+void CBordersPlusPlus::draw(CMonitor *pMonitor, float a, const Vector2D &offset) {
     if (!g_pCompositor->windowValidMapped(m_pWindow))
         return;
 
     if (!m_pWindow->m_sSpecialRenderData.decorate)
         return;
 
-    static auto* const PCOLOR1     = &HyprlandAPI::getConfigValue(PHANDLE, "plugin:borders-plus-plus:col.border_1")->intValue;
-    static auto* const PCOLOR2     = &HyprlandAPI::getConfigValue(PHANDLE, "plugin:borders-plus-plus:col.border_2")->intValue;
-    static auto* const PBORDERS    = &HyprlandAPI::getConfigValue(PHANDLE, "plugin:borders-plus-plus:add_borders")->intValue;
-    static auto* const PROUNDING   = &HyprlandAPI::getConfigValue(PHANDLE, "decoration:rounding")->intValue;
-    static auto* const PBORDERSIZE = &HyprlandAPI::getConfigValue(PHANDLE, "general:border_size")->intValue;
+    // Configuration values
+    static auto *const PCOLOR1 = &HyprlandAPI::getConfigValue(PHANDLE, "plugin:borders-plus-plus:col.border_1")->intValue;
+    static auto *const PCOLOR2 = &HyprlandAPI::getConfigValue(PHANDLE, "plugin:borders-plus-plus:col.border_2")->intValue;
+    static auto *const PBORDERS = &HyprlandAPI::getConfigValue(PHANDLE, "plugin:borders-plus-plus:add_borders")->intValue;
+    static auto *const PROUNDING = &HyprlandAPI::getConfigValue(PHANDLE, "decoration:rounding")->intValue;
+    static auto *const PBORDERSIZE = &HyprlandAPI::getConfigValue(PHANDLE, "general:border_size")->intValue;
+    static auto *const BORDERRATIO = &HyprlandAPI::getConfigValue(PHANDLE, "plugin:borders-plus-plus:border_ratio")->intValue;
+    const auto BORDER_SIZE = *PBORDERSIZE * *BORDERRATIO;
 
     if (*PBORDERS < 1)
         return;
 
-    const auto ROUNDING = !m_pWindow->m_sSpecialRenderData.rounding ?
-        0 :
-        (m_pWindow->m_sAdditionalConfigData.rounding.toUnderlying() == -1 ? *PROUNDING : m_pWindow->m_sAdditionalConfigData.rounding.toUnderlying());
+    const auto ROUNDING = !m_pWindow->m_sSpecialRenderData.rounding
+        ? 0
+        : (m_pWindow->m_sAdditionalConfigData.rounding.toUnderlying() == -1 ? *PROUNDING : m_pWindow->m_sAdditionalConfigData.rounding.toUnderlying());
 
-    // draw the border
-    wlr_box fullBox = {(int)(m_vLastWindowPos.x - *PBORDERSIZE), (int)(m_vLastWindowPos.y - *PBORDERSIZE), (int)(m_vLastWindowSize.x + 2.0 * *PBORDERSIZE),
-                       (int)(m_vLastWindowSize.y + 2.0 * *PBORDERSIZE)};
+    // Draw the border
+    wlr_box fullBox = {
+        (int)(m_vLastWindowPos.x - *PBORDERSIZE),
+        (int)(m_vLastWindowPos.y - *PBORDERSIZE),
+        (int)(m_vLastWindowSize.x + 2.0 * *PBORDERSIZE),
+        (int)(m_vLastWindowSize.y + 2.0 * *PBORDERSIZE)
+    };
 
     fullBox.x -= pMonitor->vecPosition.x;
     fullBox.y -= pMonitor->vecPosition.y;
 
-    m_seExtents = {{m_vLastWindowPos.x - fullBox.x - pMonitor->vecPosition.x + 2, m_vLastWindowPos.y - fullBox.y - pMonitor->vecPosition.y + 2},
-                   {fullBox.x + fullBox.width + pMonitor->vecPosition.x - m_vLastWindowPos.x - m_vLastWindowSize.x + 2,
-                    fullBox.y + fullBox.height + pMonitor->vecPosition.y - m_vLastWindowPos.y - m_vLastWindowSize.y + 2}};
+    m_seExtents = {
+        {
+            m_vLastWindowPos.x - fullBox.x - pMonitor->vecPosition.x + 2 + BORDER_SIZE,
+            m_vLastWindowPos.y - fullBox.y - pMonitor->vecPosition.y + 2 + BORDER_SIZE
+        },
+        {
+            fullBox.x + fullBox.width + pMonitor->vecPosition.x - m_vLastWindowPos.x - m_vLastWindowSize.x + 2 + BORDER_SIZE,
+            fullBox.y + fullBox.height + pMonitor->vecPosition.y - m_vLastWindowPos.y - m_vLastWindowSize.y + 2 + BORDER_SIZE
+        }
+    };
 
     fullBox.x += offset.x;
     fullBox.y += offset.y;
 
     if (fullBox.width < 1 || fullBox.height < 1)
-        return; // don't draw invisible shadows
+        return; // Don't draw invisible shadows
 
-    g_pHyprOpenGL->scissor((wlr_box*)nullptr);
+    g_pHyprOpenGL->scissor((wlr_box *)nullptr);
 
     scaleBox(&fullBox, pMonitor->scale);
-    g_pHyprOpenGL->renderBorder(&fullBox, CColor(*PCOLOR1), *PROUNDING == 0 ? 0 : *PROUNDING * pMonitor->scale + *PBORDERSIZE * 2, *PBORDERSIZE, a);
+    g_pHyprOpenGL->renderBorder(&fullBox, CColor(*PCOLOR1), *PROUNDING == 0 ? 0 : BORDER_SIZE * pMonitor->scale + BORDER_SIZE * 2, BORDER_SIZE, a);
 
-    // pass 2
-
+    // Pass 2
     if (*PBORDERS < 2)
         return;
 
-    fullBox = {(int)(m_vLastWindowPos.x - *PBORDERSIZE * 2), (int)(m_vLastWindowPos.y - *PBORDERSIZE * 2), (int)(m_vLastWindowSize.x + 2.0 * *PBORDERSIZE * 2),
-               (int)(m_vLastWindowSize.y + 2.0 * *PBORDERSIZE * 2)};
+    fullBox = {
+        (int)(m_vLastWindowPos.x - BORDER_SIZE * 2),
+        (int)(m_vLastWindowPos.y - BORDER_SIZE * 2),
+        (int)(m_vLastWindowSize.x + 2.0 * BORDER_SIZE * 2),
+        (int)(m_vLastWindowSize.y + 2.0 * BORDER_SIZE * 2)
+    };
 
     fullBox.x -= pMonitor->vecPosition.x;
     fullBox.y -= pMonitor->vecPosition.y;
 
-    m_seExtents = {{m_vLastWindowPos.x - fullBox.x - pMonitor->vecPosition.x + 2, m_vLastWindowPos.y - fullBox.y - pMonitor->vecPosition.y + 2},
-                   {fullBox.x + fullBox.width + pMonitor->vecPosition.x - m_vLastWindowPos.x - m_vLastWindowSize.x + 2,
-                    fullBox.y + fullBox.height + pMonitor->vecPosition.y - m_vLastWindowPos.y - m_vLastWindowSize.y + 2}};
+    m_seExtents = {
+        {
+            m_vLastWindowPos.x - fullBox.x - pMonitor->vecPosition.x + 2 + BORDER_SIZE,
+            m_vLastWindowPos.y - fullBox.y - pMonitor->vecPosition.y + 2 + BORDER_SIZE
+        },
+        {
+            fullBox.x + fullBox.width + pMonitor->vecPosition.x - m_vLastWindowPos.x - m_vLastWindowSize.x + 2 + BORDER_SIZE,
+            fullBox.y + fullBox.height + pMonitor->vecPosition.y - m_vLastWindowPos.y - m_vLastWindowSize.y + 2 + BORDER_SIZE
+        }
+    };
 
     fullBox.x += offset.x;
     fullBox.y += offset.y;
 
     if (fullBox.width < 1 || fullBox.height < 1)
-        return; // don't draw invisible shadows
+        return; // Don't draw invisible shadows
 
-    g_pHyprOpenGL->scissor((wlr_box*)nullptr);
+    g_pHyprOpenGL->scissor((wlr_box *)nullptr);
 
     scaleBox(&fullBox, pMonitor->scale);
-    g_pHyprOpenGL->renderBorder(&fullBox, CColor(*PCOLOR2), *PROUNDING == 0 ? 0 : *PROUNDING * pMonitor->scale + *PBORDERSIZE * 4, *PBORDERSIZE, a);
+    g_pHyprOpenGL->renderBorder(&fullBox, CColor(*PCOLOR2), *PROUNDING == 0 ? 0 : *PROUNDING * pMonitor->scale + *PBORDERSIZE * 4, BORDER_SIZE, a);
 }
 
 eDecorationType CBordersPlusPlus::getDecorationType() {
     return DECORATION_CUSTOM;
 }
 
-void CBordersPlusPlus::updateWindow(CWindow* pWindow) {
+void CBordersPlusPlus::updateWindow(CWindow *pWindow) {
     const auto PWORKSPACE = g_pCompositor->getWorkspaceByID(pWindow->m_iWorkspaceID);
-
     const auto WORKSPACEOFFSET = PWORKSPACE && !pWindow->m_bPinned ? PWORKSPACE->m_vRenderOffset.vec() : Vector2D();
-
-    m_vLastWindowPos  = pWindow->m_vRealPosition.vec() + WORKSPACEOFFSET;
+    m_vLastWindowPos = pWindow->m_vRealPosition.vec() + WORKSPACEOFFSET;
     m_vLastWindowSize = pWindow->m_vRealSize.vec();
-
     damageEntire();
 }
 
 void CBordersPlusPlus::damageEntire() {
-    wlr_box dm = {(int)(m_vLastWindowPos.x - m_seExtents.topLeft.x), (int)(m_vLastWindowPos.y - m_seExtents.topLeft.y),
-                  (int)(m_vLastWindowSize.x + m_seExtents.topLeft.x + m_seExtents.bottomRight.x), (int)m_seExtents.topLeft.y};
+    wlr_box dm = {
+        (int)(m_vLastWindowPos.x - m_seExtents.topLeft.x),
+        (int)(m_vLastWindowPos.y - m_seExtents.topLeft.y),
+        (int)(m_vLastWindowSize.x + m_seExtents.topLeft.x + m_seExtents.bottomRight.x),
+        (int)m_seExtents.topLeft.y
+    };
     g_pHyprRenderer->damageBox(&dm);
 }
--- borders-plus-plus/main.cpp
+++ borders-plus-plus/main.cpp
@@ -1,12 +1,10 @@
 #define WLR_USE_UNSTABLE
 
 #include <unistd.h>
-
 #include <any>
 #include <hyprland/src/Compositor.hpp>
 #include <hyprland/src/Window.hpp>
 #include <hyprland/src/config/ConfigManager.hpp>
-
 #include "borderDeco.hpp"
 #include "globals.hpp"
 
@@ -16,7 +14,7 @@ APICALL EXPORT std::string PLUGIN_API_VERSION() {
 }
 
 void onNewWindow(void* self, std::any data) {
-    // data is guaranteed
+    // Data is guaranteed.
     auto* const PWINDOW = std::any_cast<CWindow*>(data);
 
     HyprlandAPI::addNotification(PHANDLE, "a", CColor{0.2, 1.0, 0.2, 1.0}, 2000);
@@ -30,10 +28,11 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     HyprlandAPI::addConfigValue(PHANDLE, "plugin:borders-plus-plus:add_borders", SConfigValue{.intValue = 1});
     HyprlandAPI::addConfigValue(PHANDLE, "plugin:borders-plus-plus:col.border_1", SConfigValue{.intValue = configStringToInt("rgba(000000ee)")});
     HyprlandAPI::addConfigValue(PHANDLE, "plugin:borders-plus-plus:col.border_2", SConfigValue{.intValue = configStringToInt("rgba(000000ee)")});
+    HyprlandAPI::addConfigValue(PHANDLE, "plugin:borders-plus-plus:border_ratio", SConfigValue{.intValue = 1});
 
     HyprlandAPI::registerCallbackDynamic(PHANDLE, "openWindow", [&](void* self, std::any data) { onNewWindow(self, data); });
 
-    // add deco to existing windows
+    // Add decoration to existing windows.
     for (auto& w : g_pCompositor->m_vWindows) {
         if (w->isHidden() || !w->m_bIsMapped)
             continue;
@@ -51,5 +50,5 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
 }
 
 APICALL EXPORT void PLUGIN_EXIT() {
-    ;
+    // Empty exit function.
 }
