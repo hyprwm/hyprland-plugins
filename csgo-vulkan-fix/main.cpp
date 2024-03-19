4c9d83b981ad4668b89b8a3dc24d6f3ea7ad08fd
Vaxry <vaxry@vaxry.net>
2024-03-19 22:18:47 +0000

    csgo-vulkan-fix: add commit guard

--- csgo-vulkan-fix/main.cpp
+++ csgo-vulkan-fix/main.cpp
@@ -95,6 +95,14 @@ CRegion hkWLSurfaceDamage(CWLSurface* thisptr) {
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     PHANDLE = handle;
 
+    const std::string HASH = __hyprland_api_get_hash();
+
+    if (HASH != GIT_COMMIT_HASH) {
+        HyprlandAPI::addNotification(PHANDLE, "[csgo-vulkan-fix] Failure in initialization: Version mismatch (headers ver is not equal to running hyprland ver)",
+                                     CColor{1.0, 0.2, 0.2, 1.0}, 5000);
+        throw std::runtime_error("[vkfix] Version mismatch");
+    }
+
     HyprlandAPI::addConfigValue(PHANDLE, "plugin:csgo-vulkan-fix:res_w", Hyprlang::INT{1680});
     HyprlandAPI::addConfigValue(PHANDLE, "plugin:csgo-vulkan-fix:res_h", Hyprlang::INT{1050});
     HyprlandAPI::addConfigValue(PHANDLE, "plugin:csgo-vulkan-fix:class", Hyprlang::STRING{"cs2"});
