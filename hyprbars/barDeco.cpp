1eb22527932127666f9409debf4c962bfb0810cc
Khalid J. <khalid.y96@outlook.com>
2025-04-05 23:34:15 +0300

    skip checking for hw cursors

--- hyprbars/barDeco.cpp
+++ hyprbars/barDeco.cpp
@@ -115,8 +115,7 @@ void CHyprBar::onTouchDown(SCallbackInfo& info, ITouch::SDownEvent e) {
 void CHyprBar::onMouseMove(Vector2D coords) {
     // ensure proper redraws of button icons on hover when using hardware cursors
     static auto* const PICONONHOVER    = (Hyprlang::INT* const*)HyprlandAPI::getConfigValue(PHANDLE, "plugin:hyprbars:icon_on_hover")->getDataStaticPtr();
-    static auto* const PHARDWARECURSOR = (Hyprlang::INT* const*)HyprlandAPI::getConfigValue(PHANDLE, "cursor:no_hardware_cursors")->getDataStaticPtr();
-    if (**PICONONHOVER && **PHARDWARECURSOR != 1)
+    if (**PICONONHOVER)
         damageOnButtonHover();
 
     if (!m_bDragPending || m_bTouchEv || !validMapped(m_pWindow))
