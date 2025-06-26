b4dcd65b9b09e5026aa3c40fc76aa6977c530223
Tom Englund <tomenglund26@gmail.com>
2025-06-26 17:16:21 +0200

    hyprtrails: move to gles3 shadering
    now that we use gles3 in hyprland, move over the shader to gles3 in
    hyprtrails too.

--- hyprtrails/shaders.hpp
+++ hyprtrails/shaders.hpp
@@ -3,13 +3,15 @@
 #include <string>
 
 inline const std::string QUADTRAIL = R"#(
+#version 300 es
+precision mediump float;
 uniform mat3 proj;
 uniform vec4 color;
-attribute vec2 pos;
-attribute vec2 texcoord;
-attribute vec4 colors;
-varying vec4 v_color;
-varying vec2 v_texcoord;
+in vec2 pos;
+in vec2 texcoord;
+in vec4 colors;
+out vec4 v_color;
+out vec2 v_texcoord;
 
 void main() {
     gl_Position = vec4(proj * vec3(pos, 1.0), 1.0);
@@ -18,11 +20,14 @@ void main() {
 })#";
 
 inline const std::string FRAGTRAIL = R"#(
+#version 300 es
 precision mediump float;
-varying vec4 v_color;
-varying vec2 v_texcoord;
+in vec4 v_color;
+in vec2 v_texcoord;
 
 uniform vec4 window;
+
+layout(location = 0) out vec4 fragColor;
 
 float distToRect(vec4 rect) {
     float dx = max(rect[0] - v_texcoord[0], max(0.0, v_texcoord[0] - rect[2]));
@@ -45,10 +50,10 @@ float alphaForShot(vec4 shot, float threshold) {
 
 void main() {
 
-	vec4 pixColor = v_color;
+    vec4 pixColor = v_color;
     float a = v_color[3]; // clamp(alphaForShot(window, 0.5), 0.0, 1.0); // todo
 
     pixColor.rgb *= a;
 
-	gl_FragColor = pixColor;
+    fragColor = pixColor;
 })#";
