14f9a444793d6dd78c29033acf9c3c974ded708d
Malix <alixbrunetcontact@gmail.com>
2025-07-20 15:47:49 +0200

    enhance: official plugins links

--- csgo-vulkan-fix/default.nix
+++ csgo-vulkan-fix/default.nix
@@ -11,7 +11,7 @@ hyprlandPlugins.mkHyprlandPlugin hyprland {
   inherit (hyprland) nativeBuildInputs;
 
   meta = with lib; {
-    homepage = "https://github.com/hyprwm/hyprland-plugins";
+    homepage = "https://github.com/hyprwm/hyprland-plugins/tree/main/csgo-vulkan-fix";
     description = "Hyprland CS:GO Vulkan fix";
     license = licenses.bsd3;
     platforms = platforms.linux;
--- hyprwinwrap/default.nix
+++ hyprwinwrap/default.nix
@@ -11,7 +11,7 @@ hyprlandPlugins.mkHyprlandPlugin hyprland {
   inherit (hyprland) nativeBuildInputs;
 
   meta = with lib; {
-    homepage = "https://github.com/hyprwm/hyprland-plugins";
+    homepage = "https://github.com/hyprwm/hyprland-plugins/tree/main/hyprwinwrap";
     description = "Hyprland version of xwinwrap";
     license = licenses.bsd3;
     platforms = platforms.linux;
