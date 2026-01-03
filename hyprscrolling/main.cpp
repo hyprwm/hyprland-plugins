eeb8d6c534be530c0e9ceb7f989703fb0c51cf03
KAGEYAM4 <75798544+KAGEYAM4@users.noreply.github.com>
2026-01-04 01:42:05 +0530

    hyprscrolling: remove notification on successfull load (#582)
    Generating notification on failure only.

--- hyprscrolling/main.cpp
+++ hyprscrolling/main.cpp
@@ -51,9 +51,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     HyprlandAPI::addConfigValue(PHANDLE, "plugin:hyprscrolling:explicit_column_widths", Hyprlang::STRING{"0.333, 0.5, 0.667, 1.0"});
     HyprlandAPI::addLayout(PHANDLE, "scrolling", g_pScrollingLayout.get());
 
-    if (success)
-        HyprlandAPI::addNotification(PHANDLE, "[hyprscrolling] Initialized successfully!", CHyprColor{0.2, 1.0, 0.2, 1.0}, 5000);
-    else {
+    if (!success) {
         HyprlandAPI::addNotification(PHANDLE, "[hyprscrolling] Failure in initialization: failed to register dispatchers", CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
         throw std::runtime_error("[hs] Dispatchers failed");
     }
